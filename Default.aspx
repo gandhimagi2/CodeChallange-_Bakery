e45ceff8b4a280faeabd77c84a40c17a58682a3c
Margi Gandhi <margi.gandhi@acclimation.com.au>
2021-03-22 12:19:22 +1100

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CodeChallange__Bakery.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Bakery</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="order-details-booking" >
+
+        <table align="center" class="auto-style1">
+            <tr>
+                <td class="auto-style2" colspan="2"><strong>Bakery</strong></td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Select Item</td>
+                <td class="auto-style3">
+                    <asp:DropDownList ID="DropDownListID" AppendDataBoundItems="true" runat="server">
+                     <asp:ListItem Text="Vegemite Scroll" Value="VS5" />
+                     <asp:ListItem Text="Blueberry Muffin" Value="MB11" />
+                     <asp:ListItem Text="Croissant" Value="CF" />
+                </asp:DropDownList>
+                </td>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+            <tr>
+                <td class="auto-style4">Enter Quntity</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style3" colspan="2">
+                    <asp:Button ID="Button1" runat="server" Height="24px" Text="Submit" Width="93px" BackColor="Lightgray" BorderStyle="Solid" OnClick="Button1_Click" />
+                &nbsp;
+                   <%-- <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>--%>
+                &nbsp;&nbsp;&nbsp;
+                    <%--<asp:Button ID="Button2" runat="server" BackColor="#CC00FF" BorderColor="Black" BorderStyle="Solid" Text="Login" />--%>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+        <div class="auto-style2" runat="server" id="divError" visible="true">
+            <div class="alert alert-danger" role="alert">
+                    <asp:Label runat="server" ID="Label1"> Result</asp:Label>
+                </div>
+            <br />
+                <div class="alert alert-danger" role="alert">
+                    <asp:Label runat="server" ID="lblError"></asp:Label>
+                </div>
+            </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeChallange__Bakery
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = true;
+            string selecteditem = DropDownListID.SelectedValue;
+            int quantity = Convert.ToInt32( TextBox3.Text);
+            string result=string.Empty;
+            if (selecteditem == "VS5")
+            {
+                result = quantity + " VS5";
+                string test = string.Empty;
+                int remaiingunits = 0 ;
+                int units = 0;
+                double price = 0;
+                if (quantity >= 5)
+                {
+                    units = quantity / 5;
+                    remaiingunits = quantity % 5;
+                    test = test + "<br>" + units + " * 5 $8.99";
+                    price = units * 8.99;
+                }
+                if(remaiingunits>0)
+                {
+                   
+                        units = remaiingunits / 3;
+                    if (units == 0)
+                    {
+                        units = 1;
+                    }
+                       // units = quantity / 3;
+                       // remaiingunits = remaiingunits % 3;
+                        test = test + "<br>" + units + " * 3 $6.99";
+                        price = price + units * 6.99;
+                   // remaiingunits;
+                }
+                result = result + "$" + price + "<br>" + test;
+
+            }
+            else if(selecteditem == "MB11")
+            {
+                result = quantity + " MB11";
+                string test = string.Empty;
+                int remaiingunits = 0;
+                int units = 0;
+                double price = 0;
+                if (quantity >= 8)
+                {
+                    units = quantity / 8;
+                    remaiingunits = quantity % 8;
+                    test = test + "<br>" + units + " * 8 $24.95";
+                    price = units * 24.95;
+                }
+                if (remaiingunits >= 5)
+                {
+                    //units = quantity / 3;
+                    if (remaiingunits % 5 == 0)
+                    {
+                        units = remaiingunits / 5;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        remaiingunits = 0;
+                        test = test + "<br>" + units + " * 5 $16.95";
+                        price = units * 16.95;
+                    }
+                    else if(remaiingunits%2==0)
+                    {
+                        units = remaiingunits / 2;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        // units = quantity / 3;
+                         remaiingunits = 0;
+                        test = test + "<br>" + units + " * 2 $9.95";
+                        price = price + units * 9.95;
+                    }
+                    else
+                    {
+                        units = remaiingunits / 3;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        remaiingunits = quantity % 3;
+                        test = test + "<br>" + units + " * 3 $16.95";
+                        price = units * 16.95;
+                    }
+
+                }
+                if (remaiingunits > 0)
+                {
+
+                    units = remaiingunits / 2;
+                    if (units == 0)
+                    {
+                        units = 1;
+                    }
+                    // units = quantity / 3;
+                    // remaiingunits = remaiingunits % 3;
+                    test = test + "<br>" + units + " * 2 $9.95";
+                    price = price + units * 9.95;
+                    // remaiingunits;
+                }
+                result = result + "$" + price + "<br>" + test;
+            }
+            else if (selecteditem == "CF")
+            {
+                result = quantity + " CF";
+                string test = string.Empty;
+                int remaiingunits = 0;
+                int units = 0;
+                double price = 0;
+                int totalunit = 0;
+
+                if (quantity >= 9)
+                {
+                    units = quantity / 9;
+                    remaiingunits = quantity % 9;
+                    test = test + "<br>" + units + " * 9 $16.99";
+                    totalunit = totalunit + units * 9;
+                    price = units * 16.99;
+                }
+                if (remaiingunits >= 5)
+                {
+                    //units = quantity / 3;
+                    if (remaiingunits % 5 == 0)
+                    {
+                        units = remaiingunits / 5;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        remaiingunits = 0;
+                        test = test + "<br>" + units + " * 5 $9.95";
+                        totalunit = totalunit + units * 5;
+                        price = units * 9.95;
+                    }
+                    else if (remaiingunits % 3 == 0)
+                    {
+                        units = remaiingunits / 3;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        // units = quantity / 3;
+                        remaiingunits = 0;
+                        test = test + "<br>" + units + " * 3 $5.95";
+                        totalunit = totalunit + units * 3;
+                        price = price + units * 5.95;
+                    }
+                    else
+                    {
+                        units = remaiingunits / 5;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        remaiingunits = quantity % 3;
+                        test = test + "<br>" + units + " * 5 $9.95";
+                        price = units * 16.95;
+                        totalunit = totalunit + units * 5;
+                        }
+
+                }
+                if (remaiingunits > 0)
+                {
+
+                    units = remaiingunits / 3;
+                    if (units == 0)
+                    {
+                        units = 1;
+                    }
+                    // units = quantity / 3;
+                    // remaiingunits = remaiingunits % 3;
+                    test = test + "<br>" + units + " * 3 $5.95";
+                    price = price + units * 9.95;
+                    totalunit = totalunit + units * 3;
+                    // remaiingunits;
+                }
+                result = result + " $" + price + "<br>" + test;
+                if (totalunit != quantity)
+                {
+                    result = string.Empty;
+                    result = quantity + " MB11";
+                    test = string.Empty;
+                    remaiingunits = 0;
+                    totalunit = 0;
+                    units = 0;
+                    price = 0;
+                    totalunit = 0;
+                    if (quantity >= 5)
+                    {
+                        units = quantity / 5;
+                        remaiingunits = quantity % 5;
+                        test = test + "<br>" + units + " * 5 $9.95";
+                        price = units * 8.99;
+                        totalunit = totalunit + units * 5;
+
+                    }
+                    if (remaiingunits > 0)
+                    {
+
+                        units = remaiingunits / 3;
+                        if (units == 0)
+                        {
+                            units = 1;
+                        }
+                        // units = quantity / 3;
+                        // remaiingunits = remaiingunits % 3;
+                        test = test + "<br>" + units + " * 3 $5.95";
+                        price = price + units * 6.99;
+                        totalunit = totalunit + units * 3;
+                        // remaiingunits;
+                    }
+                    result = result + " $" + price + "<br>" + test;
+
+                }
+
+            }
+            lblError.Text = result;
+        }
+    }
+}
